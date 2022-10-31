af85bdd0fd19091a800084305df9ee104849229d
txemabasleiro@hotmail.com <txemita.balseiro@gmail.com>
2022-10-31 17:30:19 +0100

    massive client emulator

---
+++ src/MassiveClient/MassiveClient/MassiveClient/Program.cs
@@ -0,0 +1,95 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int clientNumber = 0;
+
+
+Console.WriteLine("Massive client conections in C#");
+Console.WriteLine("--------------------------------");
+Console.WriteLine("Enter the number of clients to connect to the web");
+clientNumber = Convert.ToInt32(Console.ReadLine());
+
+
+
+var recurisveClient = new CustomClients();
+recurisveClient.GetInParallel(clientNumber);
+
+bool exit = true;
+
+while (exit)
+{
+
+    Task.Delay(5 * 1000).ContinueWith(t => Console.WriteLine(" ----- Starting new clients ----"));
+    Task.Delay(5 * 1000).ContinueWith(t => recurisveClient.GetInParallel(clientNumber));
+}
+
+
+
+
+
+
+
+
+
+
+
+public class CustomClients
+{
+
+    private HttpClient client;
+
+    public CustomClients()
+    {
+        client = new HttpClient();               
+    }
+        
+    public async Task<string> CallWeb(int ids)
+    {
+        Console.WriteLine("Calling client {0}", ids + 1);
+
+        //var url = "https://avengers.redglacier-bf373bf2.westeurope.azurecontainerapps.io/missions";
+        var url = "https://stackoverflow.com/questions/13740629/how-do-i-trigger-a-method-to-run-after-x-seconds";
+
+        var response = await client
+            .GetAsync(url)
+            .ConfigureAwait(false);
+
+        var res = await response.Content.ReadAsStringAsync();
+
+       
+
+        Console.WriteLine("Response client {0}", ids + 1 );
+
+        return res;
+    }
+
+    public async void GetInParallel(int clientNumbers)
+    {
+        var tasks = new List<Task<string>>();
+
+        for (int i = 0; i < clientNumbers; i++)
+        {
+            tasks.Add(CallWeb(i));
+        }
+
+        var results = await Task.WhenAll(tasks);
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
