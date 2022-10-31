46031cbd905bc1cf21514928e58708218102b475
Manuel Sánchez <manuss20@gmail.com>
2022-10-31 17:16:15 +0100

    New feature

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 # Avengers-DARP-Container-Apps
 
-
-
-
 dapr run --app-id Avengers --app-port 5000 --dapr-http-port 3500 dotnet run
 dapr run --app-id Missions --app-port 4998 --dapr-http-port 3501 dotnet run
 dapr run --app-id Payment --app-port 4999 --dapr-http-port 3502 dotnet run
--- src/Avengers.MissionApi/Program.cs
+++ src/Avengers.MissionApi/Program.cs
@@ -14,10 +14,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+var statusprogess = new[] { "Completed", "In progress", "Canceled"};
+
 var missionsFaker = new Faker<Mission>()
     .StrictMode(true)
     .RuleFor(m => m.Id, (f, m) => f.Database.Random.Guid())
-    .RuleFor(m => m.Status, (f, m) => f.Finance.TransactionType())
+    .RuleFor(m => m.Status, (f, m) => f.PickRandom(statusprogess))
+    .RuleFor(m => m.PaymentStatus, (f, m) => f.Finance.TransactionType())
     .RuleFor(m => m.Currency, (f, m) => f.Finance.Currency().Symbol)
     .RuleFor(m => m.Description, (f, m) => f.Lorem.Paragraphs(1));
 
@@ -33,6 +36,7 @@ public class Mission
 {
     public Guid Id => Guid.NewGuid();
     public string? Status { get; set; }
+    public string? PaymentStatus { get; set; }
     public string? Currency { get; set; }
     public string? Description {get; set; }
     
